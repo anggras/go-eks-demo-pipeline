a8cae03e1dda67cb0f4fa7ee1bd927cfbae6131c
Anggra Sastriawan <anggra@doit.com>
2023-12-07 03:56:43 +0000

    Updated message

--- app/main.go
+++ app/main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 )
 
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
-        fmt.Fprintf(w, "Commit hash: COMMIT_TAG\n")
-    })
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello there, you've requested: %s\n", r.URL.Path)
+		fmt.Fprintf(w, "Commit hash: COMMIT_TAG\n")
+	})
 
-    http.ListenAndServe(":8000", nil)
+	http.ListenAndServe(":8000", nil)
 }
