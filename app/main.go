73858e32c56e6fafcf76db8be859d7f48c1bdf87
Anggra Sastriawan <anggra@doit.com>
2023-12-07 10:10:23 +0700

    Updated commit tag variable name

--- app/main.go
+++ app/main.go
@@ -8,7 +8,7 @@ import (
 func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
-        fmt.Fprintf(w, "Commit hash: COMMIT_HASH\n")
+        fmt.Fprintf(w, "Commit hash: COMMIT_TAG\n")
     })
 
     http.ListenAndServe(":8000", nil)
